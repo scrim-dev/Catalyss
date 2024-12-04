6a7e9192b81f2fa6da6b9c278100dbfad08d635e
Scrim <105956702+scrim-dev@users.noreply.github.com>
2024-12-04 08:59:38 -0400

    Melonloader support

---
+++ Catalyss.ML/Loader.cs
@@ -0,0 +1,76 @@
+﻿using Catalyss.ML;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+[assembly: MelonInfo(typeof(Loader), "Catalyss Loader", "1.0", "Scrimmane (Scrim)")]
+[assembly: MelonGame("KisSoft", "ATLYSS")]
+[assembly: MelonPriority(0)]
+[assembly: MelonColor(ConsoleColor.Magenta)]
+[assembly: MelonAuthorColor(ConsoleColor.DarkMagenta)]
+
+namespace Catalyss.ML
+{
+    public class Loader : MelonMod
+    {
+        private static string dllLoc = $"{MelonUtils.GameDirectory}\\Catalyss";
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Loader ready!");
+
+            if (!Directory.Exists(dllLoc))
+            {
+                try { Directory.CreateDirectory(dllLoc); } 
+                catch { MelonLogger.Error("Failed to load directory."); }
+            }
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            LoadDll($"{dllLoc}\\CatalyssMod.dll");
+        }
+
+        public override void OnApplicationQuit()
+        {
+            Process.GetCurrentProcess().Kill(); //Faster close
+        }
+
+        internal static void LoadDll(string dllPath)
+        {
+            MelonLogger.Msg("Loader Initialized!");
+
+            if (File.Exists(dllPath))
+            {
+                try
+                {
+                    Assembly loadedAssembly = Assembly.LoadFile(dllPath);
+
+                    Type entryType = loadedAssembly.GetType("CatalyssMod.Entry");
+                    MethodInfo entryMethod = entryType?.GetMethod("Load", BindingFlags.Public | BindingFlags.Static);
+
+                    if (entryMethod != null)
+                    {
+                        MelonLogger.Msg($"Found and invoking entry point.");
+                        entryMethod.Invoke(null, null);
+                    }
+                    else
+                    {
+                        MelonLogger.Warning($"No entry point found. Loaded without invoking.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Error($"Failed to load {dllPath}: {ex.Message}");
+                }
+            }
+            else
+            {
+                MelonLogger.Error($"DLL not found: {dllPath}");
+            }
+        }
+    }
+}
---
+++ Catalyss.ML/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Catalyss.ML")]
+[assembly: AssemblyDescription("Melonloader loader dll for Catalyss")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Scrimmane")]
+[assembly: AssemblyProduct("Catalyss.ML")]
+[assembly: AssemblyCopyright("Copyright Scrimmane ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c32835c0-a11f-4bcc-9853-826f370de52c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
